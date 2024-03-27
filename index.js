f39d0ed432c983253c12c51a2c154655da866a0e
davidmugambi104 <davidmugambi104@gmail.com>
2024-03-27 19:50:36 +0300

    control-flow-lab

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// index.js
+
+// Function to calculate charges based on distance
+function scuberGreetingForFeet(feet) {
+  let result;
+  if (feet <= 400) {
+      result = 'This one is on me!';
+  } else if (feet > 400 && feet <= 2000) {
+      result = 'That will be twenty bucks.';
+  } else if (feet > 2000 && feet <= 2500) {
+      result = 'I will gladly take your thirty bucks.';
+  } else {
+      result = 'No can do.';
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+// Function to check city and return message
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// Function to switch on charm based on tip
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+      case 'generous':
+          return 'Thank you so much.';
+      case 'not as generous':
+          return 'Thank you.';
+      default:
+          return 'Bye.';
+  }
 }
+
+module.exports = { scuberGreetingForFeet, ternaryCheckCity, switchOnCharmFromTip };
